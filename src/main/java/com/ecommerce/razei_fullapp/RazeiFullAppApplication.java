50f9dce77d9571ef21a4fc55d8c473264e537daa
Luis Maestre <luisaugustomaestre@gmail.com>
2024-02-06 16:20:35 -0400

    first commit

---
+++ src/main/java/com/ecommerce/razei_fullapp/RazeiFullAppApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.razei_fullapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RazeiFullAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RazeiFullAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ecommerce/razei_fullapp/RazeiFullAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.razei_fullapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RazeiFullAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
